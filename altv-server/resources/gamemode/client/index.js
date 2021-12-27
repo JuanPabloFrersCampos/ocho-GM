9cdd2b5a93e1c0e91e2611d98f3ac6774deb12fb
juanpablofrerscampos <juanpfrers@gmail.com>
2021-12-27 19:15:10 -0300

    Server & client ready to run

---
+++ altv-server/resources/gamemode/client/index.js
@@ -0,0 +1,6 @@
+// <reference types="@altv/types-client" />
+// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+import * as native from 'natives';
+
+alt.log('Hello from client'); // Only visible in 'F8' menu.
---
+++ altv-server/resources/gamemode/server/index.js
@@ -0,0 +1,4 @@
+// <reference types="@altv/types-server" />
+import * as alt from 'alt-server';
+
+alt.log('Hello from server');
